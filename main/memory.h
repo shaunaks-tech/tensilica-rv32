10e0b7d311acd2f2323305f67c443965a2cc131d
shaunaks-tech <shaunak.sarlashkar@gmail.com>
2026-05-14 14:36:35 -0400

    setup .h files

--- main/memory.h
+++ main/memory.h
@@ -1,20 +1,12 @@
-#pragma once
 #include <stdint.h>
-#include <stddef.h>
+#include <stdbool.h>
+#include "rv32i.h"
+#ifndef MEMORY_H
+#define MEMORY_H
+#define MEMORY_SIZE 65536 // 64KB
 
-#define MEM_SIZE 0x10000
-
-typedef struct rv32i_mem {
-    uint8_t ram[MEM_SIZE];
+typedef struct {
+    uint8_t data[MEMORY_SIZE];
 } rv32i_mem_t;
 
-void     mem_init(rv32i_mem_t *m);
-void     mem_load(rv32i_mem_t *m, const uint8_t *data, size_t len, uint32_t base);
-
-uint8_t  mem_read8 (rv32i_mem_t *m, uint32_t addr);
-uint16_t mem_read16(rv32i_mem_t *m, uint32_t addr);
-uint32_t mem_read32(rv32i_mem_t *m, uint32_t addr);
-
-void     mem_write8 (rv32i_mem_t *m, uint32_t addr, uint8_t  val);
-void     mem_write16(rv32i_mem_t *m, uint32_t addr, uint16_t val);
-void     mem_write32(rv32i_mem_t *m, uint32_t addr, uint32_t val);
+#endif 
--- main/rv32i.h
+++ main/rv32i.h
@@ -1,4 +1,16 @@
-#pragma once
 #include <stdint.h>
 #include <stdbool.h>
 
+#ifndef RV32I_H
+#define RV32I_H
+#define RV32I_RESET_ADDR 0x00000000
+#define NUM_REGS 32
+
+typedef struct {
+    uint32_t reg[NUM_REGS]; // 32 registers
+    uint32_t pc;
+    bool halted;
+    uint32_t cycle_count;
+} rv32i_cpu_t;
+
+#endif 
